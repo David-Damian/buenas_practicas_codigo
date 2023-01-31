fb9c25af5f1fbec706f53c3b5455063e829fc37f
David-Damian <110922245+David-Damian@users.noreply.github.com>
2023-01-31 17:05:43 -0600

    codigo para guardar graficas en dir figures

--- codigo-limpio/src/visualization/visualize.py
+++ codigo-limpio/src/visualization/visualize.py
@@ -1,18 +1,27 @@
 """"
 This script allows to user, create the objects necessary for
 an EDA. 
-Pictures are stored in the directory figures
+Pictures are stored in the directory called figures.
 """
+from matplotlib import pyplot as plt
+import seaborn as sns
+from seaborn import heatmap, countplot,violinplot,scatterplot
+import sys
+from codigo_limpio.src import load_clean
+
+#Path donde se almacenaran las graficas.
+# Abrir yaml
+with open("../../config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+FIGURES_PATH=config['visualization']['FIGURES_PATH']
+
+#Cargar conjunto de entrenamiento
+train_data = cargar_datos()[0]
 
 # Heat Map
 fig, ax = plt.subplots(figsize=(25,10))
 heat_map=sns.heatmap(data=train_data.isnull(), yticklabels=False, ax=ax)
-figure = heat_map.get_figure()    
-figure.savefig('figures/heatmap.png', dpi=400)
-
-# Violin graph of Sales Pirce by HOse Style
-fig, ax = plt.subplots(figsize=(25,10))
-sns.countplot(x=train_data['SaleCondition'])
-sns.histplot(x=train_data['SaleType'], kde=True, ax=ax)
-sns.violinplot(x=train_data['HouseStyle'], y=train_data['SalePrice'],ax=ax)
-sns.scatterplot(x=train_data["Foundation"], y=train_data["SalePrice"], palette='deep', ax=ax)
+figure = heat_map.get_figure()
+nombre_figura="heat_map"
+HEATHMAP_PATH='{} {}'.format(FIGURES_PATH, nombre_figura)
+figure.savefig(HEATHMAP_PATH, dpi=400)
