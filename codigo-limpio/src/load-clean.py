7c97ce37fb0f37da10f6c3ecf32e8c57ade78fdd
David-Damian <110922245+David-Damian@users.noreply.github.com>
2023-01-31 13:32:00 -0600

    corrigiendo paths

--- codigo-limpio/src/load-clean.py
+++ codigo-limpio/src/load-clean.py
@@ -5,12 +5,13 @@ del conjunto de validación y prueba.
 
 
 """
+import yaml
+import pandas as pd
 
-#Definimos una función para hacer la carga de datos
+#Hacer la carga de datos
 def cargar_datos():
-    ''''
+    """
     Carga de datos de entrenamiento y validación.
-    Si DROP==TRUE, 
         
     Return:
     --------
@@ -18,11 +19,20 @@ def cargar_datos():
     
     train_data (pd.DataFrame): Conjunto de datos de entrenamiento.
     test_data (pd.DataFrame): Conjunto de datos de prueba.
-    ''''
+    """
+    #Obtener directorio de trabajo actual
+    cwd = os.getcwd()
+    
+
+    # Abrir yaml
+    with open("../config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+
     # Conjunto de entrenamiento
-    train_data = pd.read_csv("house-prices-data/train.csv")
+    train_data = pd.read_csv(config['data']['TRAIN_PATH'])
+
     #Conjunto de prueba
-    test_data = pd.read_csv("house-prices-data/test.csv")
+    test_data = pd.read_csv(config['data']['TEST_PATH'])
     
     return train_data, test_data
 
@@ -40,7 +50,6 @@ def fill_datos_faltantes(data):
     train_transformed(pdDataFrame): Conjunto de datos transformado.
     ---
     '''
-    variables_incompletas=['FireplaceQu', 'BsmtQual', 'BsmtCond', 'BsmtFinType1', 'BsmtFinType1', 'BsmtFinType2']
     #Rellenar los datos NA con No
     for var in variables_incompletas:
         data[var].fillna("No", inplace=True)
