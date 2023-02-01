433ff1d8e2f45246e3b0cc29e4c59ea5a996fa40
David Damián <110922245+David-Damian@users.noreply.github.com>
2023-01-31 18:59:37 -0600

    getting better code

--- codigo-limpio/src/visualization.py
+++ codigo-limpio/src/visualization.py
@@ -5,16 +5,12 @@ Las figuras son alamcendas en el directorio figures
 del directorio padre.
 """
 from matplotlib import pyplot as plt
-from seaborn import heatmap, countplot,violinplot,scatterplot
-from load_clean import cargar_datos
 import seaborn as sns
 import yaml
-
+from load_clean import cargar_datos
 
 # Cargar conjunto de entrenamiento
 train_data = cargar_datos()[0]
-
-#
 
 # Heat Map
 fig, ax = plt.subplots(figsize=(25,10))
@@ -24,25 +20,28 @@ figure1 = heat_map.get_figure()         #guarrdamos la grafica en una variable
 # Graficas de violin por tipo de casa
 fig, ax = plt.subplots(figsize=(25,10))
 violin = sns.countplot(x=train_data['SaleCondition'])
-violin = sns.histplot(x=train_data['SaleType'], kde=True, ax=ax)
-violin = sns.violinplot(x=train_data['HouseStyle'], y=train_data['SalePrice'],ax=ax)
-violin = sns.scatterplot(x=train_data["Foundation"], y=train_data["SalePrice"], palette='deep', ax=ax)
+violin = sns.histplot(x=train_data['SaleType'],kde=True, ax=ax)
+violin = sns.violinplot(x=train_data['HouseStyle'], y=train_data['SalePrice'],
+                        ax=ax)
+violin = sns.scatterplot(x=train_data["Foundation"],
+                         y=train_data["SalePrice"],
+                         palette='deep', ax=ax)
 figure2 = violin.get_figure()           #guarrdamos la grafica en una variable
-
 # Almacenar las graficas.
 
 # Abrir yaml
-with open("../config.yaml", "r") as file:
+with open("../config.yaml", encoding="utf-8") as file:
     config = yaml.safe_load(file)
 
-FIGURES_PATH=config['visualization']['FIGURES_PATH']        #obtener ruta donde se alamcenaran las graficas
+# Obtener ruta donde se alamcenaran las graficas
+FIGURES_PATH=config['visualization']['FIGURES_PATH']
 
-nombre_figuras=["heat_map", "violin_plot"]                  #nombre de cada imagen
+nombre_figuras=["heat_map", "violin_plot"]
 
 #Guardar Heatmap
-save_heatmap='{}{}'.format(FIGURES_PATH, nombre_figuras[0])
+save_heatmap = f"{FIGURES_PATH}{nombre_figuras[0]}"
 figure1.savefig(save_heatmap, dpi=400)
 
 #Guardar violin plots
-save_violinplot='{}{}'.format(FIGURES_PATH, nombre_figuras[1])
+save_violinplot = f"{FIGURES_PATH}{nombre_figuras[1]}"
 figure2.savefig(save_violinplot, dpi=400)
