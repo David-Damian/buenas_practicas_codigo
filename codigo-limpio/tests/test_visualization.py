7af81d811b77e8ec7282e9acf910e43306bcdbf7
David-Damian <110922245+David-Damian@users.noreply.github.com>
2023-02-11 19:59:50 -0600

    cambio en assert

--- codigo-limpio/tests/test_visualization.py
+++ codigo-limpio/tests/test_visualization.py
@@ -16,20 +16,20 @@ def path():
     el retorno del path() como un argumento.
     '''
     # Abrir yaml para obtener ruta de los datos.
-    with open("../config.yaml", encoding="utf-8") as file:
+    with open("./config.yaml", encoding="utf-8") as file:
         config = yaml.safe_load(file)
     pth = config['test']['TRAIN_PATH']
     return pth
 
 def test_eda(train_path):
     try:
-        with open("../config.yaml", encoding="utf-8") as file:
+        with open("./config.yaml", encoding="utf-8") as file:
             config = yaml.safe_load(file)
         path = config['test']['FIGURES_PATH']
         # Generar graficas
         vs.eda(train_path, path)
         # Verificar que se guardaron
-        assert len(os.listdir(path)) == 3
+        assert len(os.listdir(path)) > 0
     except AssertionError as ass_err:
         logging.error("No se han guardado las gráficas.")
         raise ass_err
